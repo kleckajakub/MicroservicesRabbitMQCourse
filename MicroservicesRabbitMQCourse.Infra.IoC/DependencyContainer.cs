81738656174ea24786c2bcd6fe98035921be2179
kleckajakub <kleckajakub@gmail.com>
2023-05-01 17:07:56 +0200

    - dependency injection init commit

---
+++ MicroservicesRabbitMQCourse.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,12 @@
+﻿using MicroservicesrabbitMQCourse.Domain.Core.Events;
+using MicroservicesRabbitMQCourse.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroservicesRabbitMQCourse.Infra.IoC;
+
+public class DependencyContainer {
+  public static void RegisterServices(IServiceCollection services) {
+    // Domain Bus
+    services.AddTransient<IEventBus, RabbitMQBus>();
+  }
+}
