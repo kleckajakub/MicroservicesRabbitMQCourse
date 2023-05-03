ab46894d5732e9797d7c8b758255af34cb3daf1f
kleckajakub <kleckajakub@gmail.com>
2023-05-03 23:26:12 +0200

    - banking.api - OK (run via swagger in browser)

---
+++ MicroservicesRabbitMQCourse.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,20 @@
+﻿using MicroservicesRabbitMQCourse.Banking.Application.Interfaces;
+using MicroservicesRabbitMQCourse.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroservicesRabbitMQCourse.Banking.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BankingController : ControllerBase {
+  private readonly IAccountService accountService;
+
+  public BankingController(IAccountService accountService) {
+    this.accountService = accountService;
+  }
+
+  [HttpGet]
+  public ActionResult<IEnumerable<Account>> Get() {
+    return Ok(accountService.GetAccounts());
+  }
+}
--- MicroservicesRabbitMQCourse.Banking.Api/Program.cs
+++ MicroservicesRabbitMQCourse.Banking.Api/Program.cs
@@ -1,23 +1,22 @@
+using System.Reflection;
 using MicroservicesRabbitMQCourse.Banking.Data.Context;
 using MicroservicesRabbitMQCourse.Infra.IoC;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-       .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo {Title = "Banking Microservice", Version = "v1"}));
 builder.Services.AddDbContext<BankingDbContext>(options => {
   options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection"));
 });
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
 RegisterServices(builder.Services);
 
@@ -26,7 +25,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
   app.UseSwagger();
-  app.UseSwaggerUI();
+  app.UseSwaggerUI(cfg => cfg.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1"));
 }
 
 app.UseHttpsRedirection();
